a30738110aacd097b5185866d006de09750e3cde
Oshan Wisumperuma <warunaoshan@gmail.com>
2019-06-02 14:01:43 +0530

    code format

--- app/models/education_facility.rb
+++ app/models/education_facility.rb
@@ -1,7 +1,6 @@
 class EducationFacility < ApplicationRecord
   def self.to_json(facilities)
     data = []
-    p facilities.count
     facilities.each do |facility|
       data << {
         "geometry": JSON.parse(facility['st_asgeojson']).to_hash,
@@ -11,7 +10,6 @@ class EducationFacility < ApplicationRecord
         }
       }
     end
-    p data.count
     data.to_json
   end
 end
